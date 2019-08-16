db1e556056840676d725d0cba2afd7e3e979bc2b
NBochkarev <econ88@gmail.com>
2019-08-16 20:58:06 +0500

    Интеграционные тесты - первые наброски

--- src/integration-test/java/ru/mortihead/numbers/IncrementorIntegrationTest.java
+++ src/integration-test/java/ru/mortihead/numbers/IncrementorIntegrationTest.java
@@ -14,11 +14,13 @@ public class IncrementorIntegrationTest {
 
     @Test
     public void whenSendingGet_thenMessageIsReturned() throws IOException {
-        String url = "http://localhost:8999/service/getnumber";
+        String url = "http://localhost:8082/service/getnumber";
+        System.out.println("Try to connect and get data from "+url);
         URLConnection connection = new URL(url).openConnection();
         try (InputStream response = connection.getInputStream();
              Scanner scanner = new Scanner(response)) {
             String responseBody = scanner.nextLine();
+            System.out.println("Response: "+responseBody);
             assertEquals("0", responseBody);
         }
     }
